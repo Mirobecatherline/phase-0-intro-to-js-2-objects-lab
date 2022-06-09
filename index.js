f626f4488a4a24b417db4d566d0270641d4a2249
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-09 16:56:57 +0300

    commit tree efbf012c8cc91d03178796b70afc01f159e498cb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee={name:"catherine",streetAddress:"juja"};
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const employee1={...employee};
+    employee1[key]=value;
+    return employee1;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key){
+   employee.name="Sam";
+   employee.streetAddress="12 Broadway";
+   return employee;
+}
+function deleteFromEmployeeByKey(){
+    const employee2={...employee};
+    delete employee2.name;
+    return employee2;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee.name;
+    return employee;
+}
